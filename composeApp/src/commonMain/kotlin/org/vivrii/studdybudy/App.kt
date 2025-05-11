738dcce7abf661b451ba751d36b59a41a69fc031
vivrii <45391202+vivrii@users.noreply.github.com>
2025-05-11 20:34:22 +0800

    [add]ed progress indicator movement, play pause/repeat buttons

--- composeApp/src/commonMain/kotlin/org/vivrii/studdybudy/App.kt
+++ composeApp/src/commonMain/kotlin/org/vivrii/studdybudy/App.kt
@@ -1,24 +1,51 @@
 package org.vivrii.studdybudy
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import studdybudy.composeapp.generated.resources.Res
+import studdybudy.composeapp.generated.resources.pause
+import studdybudy.composeapp.generated.resources.play
+import studdybudy.composeapp.generated.resources.repeat
+import studdybudy.composeapp.generated.resources.repeat_off
+import studdybudy.composeapp.generated.resources.repeat_once
 
 @Composable
 @Preview
@@ -39,25 +66,111 @@ fun App() {
                     .padding(12.dp)
                     .clip(RoundedCornerShape(32.dp))
                     .background(color = MaterialTheme.colors.surface) // todo: to be a colour based on album colour + transparency effect w background
-                    .padding(24.dp)
+                    .padding(3.dp)
             ) {
                 Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                    horizontalAlignment = Alignment.Start,
                     verticalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth().padding(24.dp)
                 ) {
-                    Row {
-                        Box(modifier = Modifier.width(48.dp).height(48.dp))
-                        Text(text = "song name goes here I think", color = MaterialTheme.colors.onSurface)
+                    var playing by remember { mutableStateOf(false) }
+                    var repeatMode by remember { mutableIntStateOf(0) }
+                    var progress by remember { mutableFloatStateOf(0.7f) }
+
+                    LaunchedEffect(playing, repeatMode) {
+                        while (playing) {
+                            progress += 0.0005555556f
+                            if (progress >= 1.0f) {
+                                if (repeatMode == 0) {
+                                    playing = false
+                                } else {
+                                    progress = 0.0f
+                                }
+                            }
+                            delay(100)
+                        }
                     }
-                    Box(
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(modifier = Modifier.width(48.dp).height(48.dp).debug())
+                        Column(
+                            horizontalAlignment = Alignment.Start,
+                            verticalArrangement = Arrangement.Center,
+                            modifier = Modifier.height(48.dp).padding(horizontal = 12.dp).weight(1.0f)
+                        ) {
+                            Text(text = "Leave Me Be", color = MaterialTheme.colors.onSurface, style = TextStyle(
+                                fontSize = 16.sp,
+                            ))
+                            Text(text = "Computerwife", color = MaterialTheme.colors.onSurface, style = TextStyle(
+                                fontSize = 12.sp,
+                            ))
+                        }
+
+                        if (playing) {
+                            vectorResource(resource = Res.drawable.pause) to "pause button"
+                        } else {
+                            vectorResource(resource = Res.drawable.play) to "play button"
+                        }.let { (imageVector, contentDescription) ->
+                            Icon(
+                                imageVector = imageVector,
+                                contentDescription = contentDescription,
+                                modifier = Modifier
+                                    .size(48.dp)
+                                    .padding(3.dp)
+                                    .clickable { playing = !playing }
+                                ,
+                                tint = MaterialTheme.colors.onSurface
+                            )
+                        }
+
+                        when (repeatMode) {
+                            0 -> {
+                                Triple(
+                                    vectorResource(resource = Res.drawable.repeat_off),
+                                    "repeat off",
+                                    { repeatMode = 1 }
+                                )
+                            }
+                            1 -> {
+                                Triple(
+                                    vectorResource(resource = Res.drawable.repeat),
+                                    "repeat on",
+                                    { repeatMode = 2 }
+                                )
+                            }
+                            else -> {
+                                Triple(
+                                    vectorResource(resource = Res.drawable.repeat_once),
+                                    "repeat once",
+                                    { repeatMode = 0 }
+                                )
+                            }
+                        }.let { (imageVector, contentDescription, onClick) ->
+                            Icon(
+                                imageVector = imageVector,
+                                contentDescription = contentDescription,
+                                modifier = Modifier
+                                    .size(32.dp)
+                                    .padding(3.dp)
+                                    .clickable { onClick() },
+                                tint = MaterialTheme.colors.onSurface
+                            )
+                        }
+                    }
+
+                    LinearProgressIndicator(
+                        progress = progress,
                         modifier = Modifier
-                            .padding(24.dp)
                             .fillMaxWidth()
-                            .clip(RoundedCornerShape(12.dp))
+                            .clip(RoundedCornerShape(bottomStart = 3.dp, bottomEnd = 3.dp, topEnd = 3.dp))
                             .height(6.dp)
-                            .background(color = MaterialTheme.colors.primary)
                     )
+
+                    LaunchedEffect(repeatMode, playing) {
+
+                    }
                 }
             }
         }
