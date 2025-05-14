1ef4804dc5e11bbe647aa0a8574fcfd8b90d5b97
vivrii <45391202+vivrii@users.noreply.github.com>
2025-05-15 00:19:28 +0800

    [split] out functions for MediaPlayerUI.kt

--- composeApp/src/commonMain/kotlin/org/vivrii/studdybudy/ui/components/MediaPlayerUI.kt
+++ composeApp/src/commonMain/kotlin/org/vivrii/studdybudy/ui/components/MediaPlayerUI.kt
@@ -1,12 +1,12 @@
 package org.vivrii.studdybudy.ui.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -27,7 +27,9 @@ import androidx.compose.ui.unit.dp
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 import org.jetbrains.compose.resources.vectorResource
+import org.vivrii.studdybudy.music.MusicPlayerState
 import org.vivrii.studdybudy.music.MusicViewModel
+import org.vivrii.studdybudy.music.model.Song
 import org.vivrii.studdybudy.ui.extensions.debug
 import studdybudy.composeapp.generated.resources.Res
 import studdybudy.composeapp.generated.resources.pause
@@ -41,9 +43,28 @@ import studdybudy.composeapp.generated.resources.repeat_once
 fun MediaPlayer(viewModel: MusicViewModel) {
     val currentSong by viewModel.currentSong.collectAsState()
     val playerState by viewModel.playerState.collectAsState()
-    val progress by viewModel.progress.collectAsState()
     val repeatMode by viewModel.repeatMode.collectAsState()
+    val progress by viewModel.progress.collectAsState()
 
+    NowPlayingBar(
+        song = currentSong,
+        playerState = playerState,
+        repeatMode = repeatMode,
+        progress = progress,
+        onClickPlay = { viewModel.playPauseToggle() },
+        onClickRepeat = { viewModel.repeatModeCycle() },
+    )
+}
+
+@Composable
+fun NowPlayingBar(
+    song: Song?,
+    playerState: MusicPlayerState,
+    repeatMode: Int,
+    progress: Float,
+    onClickPlay: () -> Unit,
+    onClickRepeat: () -> Unit
+) {
     Box(
         modifier = Modifier
             .padding(12.dp)
@@ -59,76 +80,103 @@ fun MediaPlayer(viewModel: MusicViewModel) {
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                val albumArtResource = currentSong?.albumArtUri?.let { asyncPainterResource(it) }
-                albumArtResource?.let {
-                    KamelImage(
-                        resource = { it },
-                        contentDescription = " Album art",
-                        modifier = Modifier.width(48.dp).height(48.dp)
-                    )
-                } ?: run {
-                    // todo: instead of debug box, use a default uri to an included missing icon file
-                    Box(modifier = Modifier.width(48.dp).height(48.dp).debug()) {
-                }
-
-                }
-                Column(
-                    horizontalAlignment = Alignment.Start,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier.height(48.dp).padding(horizontal = 12.dp).weight(1.0f)
-                ) {
-                    Text(
-                        text = currentSong?.title ?: "--",
-                        color = MaterialTheme.colors.onSurface,
-                        style = MaterialTheme.typography.body1
-                    )
-                    Text(
-                        text = currentSong?.artist ?: "--",
-                        color = MaterialTheme.colors.onSurface,
-                        style = MaterialTheme.typography.caption
-                    )
-                }
-
-                if (playerState.isPlaying) {
-                    vectorResource(resource = Res.drawable.pause) to "pause button"
-                } else {
-                    vectorResource(resource = Res.drawable.play) to "play button"
-                }.let { (imageVector, contentDescription) ->
-                    Icon(
-                        imageVector = imageVector,
-                        contentDescription = contentDescription,
-                        modifier = Modifier
-                            .size(48.dp)
-                            .padding(3.dp)
-                            .clickable { viewModel.playPauseToggle()},
-                        tint = MaterialTheme.colors.onSurface
-                    )
-                }
-
-                when (repeatMode) {
-                    0 -> vectorResource(resource = Res.drawable.repeat_off) to "repeat off"
-                    1 -> vectorResource(resource = Res.drawable.repeat) to "repeat on"
-                    else -> vectorResource(resource = Res.drawable.repeat_once) to "repeat once"
-                }.let { (imageVector, contentDescription) ->
-                    Icon(
-                        imageVector = imageVector,
-                        contentDescription = contentDescription,
-                        modifier = Modifier
-                            .size(32.dp)
-                            .padding(3.dp)
-                            .clickable { viewModel.repeatModeCycle() },
-                        tint = MaterialTheme.colors.onSurface
-                    )
-                }
+                AlbumArtIcon(song?.albumArtUri)
+                SongInformation(song?.title, song?.artist)
+                PlayIcon(playerState.isPlaying) { onClickPlay() }
+                RepeatIcon(repeatMode) { onClickRepeat() }
             }
 
-            LinearProgressIndicator(
-                progress = progress,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(MaterialTheme.shapes.small.copy(topStart = CornerSize(0.dp)))
-                    .height(6.dp)
-            )
+            SongProgressIndicator(progress)
         }
+    }
+}
+
+@Composable
+private fun SongProgressIndicator(progress: Float) {
+    LinearProgressIndicator(
+        progress = progress,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.small.copy(topStart = CornerSize(0.dp)))
+            .height(6.dp)
+    )
+}
+
+@Composable
+private fun AlbumArtIcon(albumArtUri: String?) {
+    val albumArtResource = albumArtUri?.let { asyncPainterResource(it) }
+    albumArtResource?.let {
+        KamelImage(
+            resource = { it },
+            contentDescription = " Album art",
+            modifier = Modifier.width(48.dp).height(48.dp)
+        )
+    } ?: run {
+        // todo: instead of debug box, use a default uri to an included missing icon file
+        Box(modifier = Modifier.width(48.dp).height(48.dp).debug())
+    }
+}
+
+@Composable
+private fun RowScope.SongInformation(title: String?, artist: String?) {
+    Column(
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.height(48.dp).padding(horizontal = 12.dp).weight(1.0f)
+    ) {
+        Text(
+            text = title ?: "--",
+            color = MaterialTheme.colors.onSurface,
+            style = MaterialTheme.typography.body1
+        )
+        Text(
+            text = artist ?: "--",
+            color = MaterialTheme.colors.onSurface,
+            style = MaterialTheme.typography.caption
+        )
+    }
+}
+
+@Composable
+private fun PlayIcon(
+    isPlaying: Boolean,
+    onClick: () -> Unit
+) {
+    if (isPlaying) {
+        vectorResource(resource = Res.drawable.pause) to "pause button"
+    } else {
+        vectorResource(resource = Res.drawable.play) to "play button"
+    }.let { (imageVector, contentDescription) ->
+        Icon(
+            imageVector = imageVector,
+            contentDescription = contentDescription,
+            modifier = Modifier
+                .size(48.dp)
+                .padding(3.dp)
+                .clickable { onClick() },
+            tint = MaterialTheme.colors.onSurface
+        )
+    }
+}
+
+@Composable
+private fun RepeatIcon(
+    repeatMode: Int,
+    onClick: () -> Unit
+) {
+    when (repeatMode) {
+        0 -> vectorResource(resource = Res.drawable.repeat_off) to "repeat off"
+        1 -> vectorResource(resource = Res.drawable.repeat) to "repeat on"
+        else -> vectorResource(resource = Res.drawable.repeat_once) to "repeat once"
+    }.let { (imageVector, contentDescription) ->
+        Icon(
+            imageVector = imageVector,
+            contentDescription = contentDescription,
+            modifier = Modifier
+                .size(32.dp)
+                .padding(3.dp)
+                .clickable { onClick() },
+            tint = MaterialTheme.colors.onSurface
+        )
     }
 }
