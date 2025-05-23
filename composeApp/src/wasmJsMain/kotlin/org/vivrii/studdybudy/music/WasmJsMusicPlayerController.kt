ec4d9f971f396ff53732abaf6e1fc7892244afe2
vivrii <45391202+vivrii@users.noreply.github.com>
2025-05-23 20:26:58 +0800

    [implement]ed audio playback in wasmjs

--- composeApp/src/wasmJsMain/kotlin/org/vivrii/studdybudy/music/WasmJsMusicPlayerController.kt
+++ composeApp/src/wasmJsMain/kotlin/org/vivrii/studdybudy/music/WasmJsMusicPlayerController.kt
@@ -1,27 +1,64 @@
 package org.vivrii.studdybudy.music
 
+import kotlinx.browser.document
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import org.w3c.dom.HTMLAudioElement
+
+// todo: errors and logs
+
 class WasmJsMusicPlayerController : MusicPlayerController {
+    private lateinit var _onStateChanged: () -> Unit
+
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+
+    private var player: HTMLAudioElement = (document.createElement("audio") as HTMLAudioElement)
+
     override fun play() {
-//        TODO("Not yet implemented")
+        player.play()
     }
 
     override fun pause() {
-//        TODO("Not yet implemented")
+        player.pause()
     }
 
     override fun seekTo(positionMs: Long) {
-//        TODO("Not yet implemented")
+        player.currentTime = positionMs.div(1000.0)
     }
 
     override fun loadSong(songUri: String) {
-//        TODO("Not yet implemented")
+        player.src = songUri
+        player.load()
     }
 
     override fun repeatMode(repeat: Boolean) {
-//        TODO("Not yet implemented")
+        player.loop = repeat
     }
 
     override fun observeState(onStateChanged: (MusicPlayerState) -> Unit) {
-//        TODO("Not yet implemented")
+        _onStateChanged = {
+            onStateChanged(
+                MusicPlayerState(
+                    isPlaying = !player.paused,
+                    currentDurationMs = player.currentTime.times(1000).toLong(),
+                    totalDurationMs = player.duration.times(1000).toLong()
+                )
+            )
+        }
+
+        scope.launch {
+            while (isActive) {
+                _onStateChanged()
+                if (!player.paused) {
+                    delay(100)
+                } else {
+                    delay(250)
+                }
+            }
+        }
     }
 }
